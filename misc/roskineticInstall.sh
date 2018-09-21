88ad1082f0c9de2dbd6b1cfca949ddc52bdaaf63
Sai <ksaimouli@gmail.com>
2018-09-21 19:07:09 -0400

    Bash script to install ros kinetic on odroid

---
+++ misc/roskineticInstall.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+#AUTHOR: SAIMOULI KATRAGADDA
+#LAST UPDATED: 09/21/2018
+set -e
+
+echo "Setting up source list"
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+echo "setting up your keys" 
+
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+echo "updating"
+sudo apt-get update 
+
+#echo "will restart"
+#sudo restart now
+
+echo "insatlling ros base package"
+sudo apt-get install ros-kinetic-ros-base
+
+echo "Initializing rosdep" 
+sudo rosdep init
+rosdep update
+
+echo "Environment setup"
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+echo "Installing Dependencies" 
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+echo "you are set to use ros! GO AMAV TERPS!"
