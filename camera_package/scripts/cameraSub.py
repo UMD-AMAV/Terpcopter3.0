b26d9c35ebf457f13ab9faa35fe67f00ef053ed2
KrishnaBhatu <kbhatu@terpmail.umd.edu>
2018-12-30 16:10:10 -0500

    Added the functionality of red color detection and alignment in Subscriber node

--- camera_package/scripts/cameraSub.py
+++ camera_package/scripts/cameraSub.py
@@ -1,43 +1,12 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
+# Copyright (c) 2019, AMAV Team.
 # All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
-
-## Simple talker demo that listens to std_msgs/Strings published 
-## to the 'chatter' topic
+## Image processing code for the target detection and alignment
 
 import rospy
 import cv2
+import numpy as np
+import math
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -57,9 +26,50 @@ def imageSubscriber():
     rospy.spin()
     
 # this is the function where we can make changes to perform image processing tasks 
-def imageProcessing(image):
-    cv2.imshow('img',image)
+def imageProcessing(frame):
+    hsv_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV) #convert RGB color scheme to HSV color scheme for better color recognition
+    w,h= frame.shape[:2]
+    lower_red = np.array([0,70,50])  #set the lower bound for red
+    higher_red = np.array([10,255,250]) #set the higher bound for red
+    masking = cv2.inRange(hsv_frame, lower_red, higher_red) #with both the red color limits we detect red color from the image 
+	
+    ret,thresh = cv2.threshold(masking,127,255,0)
+    im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+    
+    if len(contours) > 0:
+		c = max(contours,key = cv2.contourArea) #finding maximum from the set of contours in the given frame
+		M = cv2.moments(c) #finding the centre of that contour
+		cX = int(M['m10'] / M['m00'])
+		cY = int(M['m01'] / M['m00'])
+		cv2.line(frame,(cX,0),(cX, w),(0,255,0),1)
+		cv2.line(frame,(0,cY),(h, cY),(0,255,0),1)
+		cv2.circle(frame,(cX,cY ), 5 ,(0,0,255),-1) #to locate the centre of frame which we assume to be quad
+		cv2.line(frame,(h/2,w/2),(cX, w/2),(0,0,255),1)
+		cv2.line(frame,(h/2,w/2),(h/2, cY),(0,0,255),1)
+		distx = math.sqrt(math.pow(h/2-cX,2))
+		disty = math.sqrt(math.pow(w/2-cY,2))
+		#cv2.drawContours(frame, contours, -1, (0,255,0), 1)
+		
+	#print distx,"  ", disty
+    distX = str(int(distx))#"{:2d}".format(dist)
+    distY = str(int(disty))
+	#The output is displayed on the frame in the form of:
+	#  (dist in X axis)   move (direction)     to reach the goal
+	#  (dist in Y axis)   move (direction)     to reach the goal
+    cv2.putText(frame,distX, (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255),2, lineType=cv2.LINE_AA)
+    cv2.putText(frame,distY, (10, 200), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255),2, lineType=cv2.LINE_AA)
+    if cX>h/2:
+        cv2.putText(frame,"move right", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255),2, lineType=cv2.LINE_AA)
+    else: 
+	cv2.putText(frame,"move left", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255),2, lineType=cv2.LINE_AA)
+    if cY>w/2:
+	cv2.putText(frame,"move down", (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255),2, lineType=cv2.LINE_AA)
+    else: 
+	cv2.putText(frame,"move up", (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255),2, lineType=cv2.LINE_AA)
+    #no need to show image while running the code
+    cv2.imshow("frames", frame)
     cv2.waitKey(3)
+    
 
 # main loop 
 if __name__ == '__main__':
