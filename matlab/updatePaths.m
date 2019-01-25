71a2862e7dbb2240bf574fe095b74295835d09c8
krawal19 <kapilrawal1995@gmail.com>
2019-01-25 15:35:06 -0500

    Added results folder containing csv file form topics  stateEstimate, PIDSetting, StickCmd, Terarangerone. Also updated paths in updatePaths.m

--- matlab/updatePaths.m
+++ matlab/updatePaths.m
@@ -13,6 +13,18 @@ addpath('./vision')
 addpath('./vision/subroutines')
 addpath('./utils')
 addpath('./utils/geometry')
+addpath('./plots')
+addpath('./plots/plotahsCmd')
+addpath('./plots/plotahsCmd/subroutines')
+addpath('./plots/plotStateEstimate')
+addpath('./plots/plotStateEstimate/subroutines')
+addpath('./plots/plotStickCmd')
+addpath('./plots/plotStickCmd/subroutines')
+addpath('./plots/plotpidSetting')
+addpath('./plots/plotpidSetting/subroutines')
+addpath('./plots/plotTerarangerone')
+addpath('./plots/plotTerarangerone/subroutines')
+addpath('./results')
 savepath;
 %savepath([params.env.matlabHome '/pathdef.m']);
 end
