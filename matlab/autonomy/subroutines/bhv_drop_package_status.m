c6cb5f6b3eb0dd533a99d81ccf2e0bb4c980beb9
Zach Lacey <zlacey1234@gmail.com>
2019-04-03 15:24:29 -0400

    working on servo drop mechanism

--- matlab/autonomy/subroutines/bhv_drop_package_status.m
+++ matlab/autonomy/subroutines/bhv_drop_package_status.m
@@ -4,7 +4,6 @@ function [completionFlag, servoCmd] = bhv_drop_package_status(completion, t)
     current_event_time = t;
   
     elapsed_satisfied_time = current_event_time - timestamps.behavior_satisfied_timestamp;
-    fprintf('Desired Yaw: %f Degrees\tCurrent Yaw %f Degrees\nDesired Time: %f\tElapsed time: %f\n', ahs.desiredYawDegrees, yaw_current, completion.durationSec, elapsed_satisfied_time);
     
     if elapsed_satisfied_time >= completion.durationSec
         completionFlag = 1;
