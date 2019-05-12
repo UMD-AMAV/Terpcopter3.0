036465501ccceae154e4ce0136bd3f60836de5e7
Artur Wolek <wolek@umd.edu>
2019-05-12 18:03:37 -0400

    river-based pitch/yaw control

--- matlab/autonomy/subroutines/bhv_hover_over_River_align.m
+++ matlab/autonomy/subroutines/bhv_hover_over_River_align.m
@@ -1,133 +1,71 @@
-function [completionFlag, ayprCmd] = bhv_hover_over_River_align(stateEstimateMsg, ayprCmd, completion, bhvTime, rDetected, rAngle, rPixelX, rPixelY, bhvLog)
+function [completionFlag, ayprCmd] = bhv_hover_over_River_align(stateEstimateMsg, ayprCmd, completion, bhvTime, rDetected, yLeft, yRight, riverLog)
 
-% hDetected = 0 (no H detected) , 1 (H detected)
-% hAngle = 0 to 180 (deg) measured  CCW from x axis East
-% hPixelX = -360 to 360 (pixels)
-% hPixelY = -640 to 640
-
-% unpack state estimate
+% rDetected
+% rPixelX = 0 to 640 (pixels)
+% rPixelY = 0 to 480
 
 
 % Note: Pitch cmd (+) = negative pitch (nose down)
 %       Roll cmd (-) = positivie roll (right wing down)
 
-persistent filtPixelX filtPixelY errorSumX errorSumY diffX diffY desiredYaw;
+persistent filtPixelY errorSumY diffY desiredYaw;
 
-% gains/params
-Kroll = 0.03/100; %
-Kdr = 0.005; %0.005; % this term will multiply ~ 1 pixel change per timestep (~0.04 sec)
-Kir = 1.5e-6; %1.5e-6
-
+% pitch gains
 Kpitch = 0.03/100;
-Kdp = 0.005; %0.005;
+Kdp = 0.005;
 Kip = 1.5e-6;
-
-Kyaw = 0.03/100;
-ySetpoint = 360; % Setpoint of River Line Midpoint
-KPitchRiver = 0.03/100;
-
-Kx_no_det = 0.02;
 Ky_no_det = 0.02;
-Kx_det = 0.2;
 Ky_det = 0.2;
 satLimit = 0.15;
 
+% yaw gains 
+ySetpoint = 120; % Setpoint of River Line Midpoint
 filtTimeConstant = 1;
 dt = 1/25;
-
-% pixelWidth = 640;
-% pixelHeight = 480;
+alpha = dt / ( filtTimeConstant + dt);
 
 % unpack state
-% z = stateEstimateMsg.Range;
-theta = stateEstimateMsg.Pitch;
-phi = stateEstimateMsg.Roll;
 yaw = stateEstimateMsg.Yaw;
 
-if isempty(filtPixelX)
-    filtPixelX = 0;
+if isempty(filtPixelY)
     filtPixelY = 0;
-    errorSumX = 0;
     errorSumY = 0;
-    diffX = 0;
     diffY = 0;
-    desiredYaw = ayprCmd.YawDesiredDegrees;
+    desiredYaw = yaw;
 end
 
-% save pixelX from previous step for computing derivative
-filtPixelXprev = filtPixelX;
 filtPixelYprev = filtPixelY;
 
-if ( hDetected )
-    filtPixelX = filtPixelX - Kx_det*(filtPixelX - hPixelX);
-    filtPixelY = filtPixelY - Ky_det*(filtPixelY - hPixelY);
+if ( rDetected )
+    % pitch to hold river distance
+    yMid = (yRight + yLeft)/2;
+    yError = yMid - ySetpoint;
+    filtPixelY = filtPixelY - Ky_det*(filtPixelY - yError);
     
+    % yaw to align with river 
+    delY = yRight - yLeft;    
+    rAngle = atand(delY,720);      
+    desiredYawCurrent = yaw + rAngle;
+    desiredYaw = (1-alpha)*desiredYaw + alpha*desiredYawCurrent;    
 else
     % converge to zero
-    filtPixelX = filtPixelX - Kx_no_det*(filtPixelX);
-    filtPixelY = filtPixelY - Ky_no_det*(filtPixelY);
-    %         % diverge
-    %         filtPixelX = filtPixelX + Kx_no_det*(filtPixelX);
-    %         filtPixelY = filtPixelY + Ky_no_det*(filtPixelY);
+    filtPixelY = filtPixelY - Ky_no_det*(filtPixelY);    
 end
 
-% integrator
-errorSumX = errorSumX + filtPixelX;
+% leaky integrator (y pixel)
 errorSumY = errorSumY + filtPixelY;
-
-% leaky integrator
-errorSumX = 0.95*errorSumX ;
 errorSumY = 0.95*errorSumY ;
 
-
-% control (P)
-% pitchCmd = -Kpitch*filtPixelY;
-% rollCmd = -Kroll*filtPixelX;
-
-% low-pass filter
-alpha = dt / ( filtTimeConstant + dt);
+% low pass filter (y pixel)
 diffY = (1-alpha)*diffY + alpha*(filtPixelY - filtPixelYprev);
-diffX = (1-alpha)*diffX + alpha*(filtPixelX - filtPixelXprev);
 
-% % control (PD)
-% % pitchCmd = -Kpitch*filtPixelY + Kdp*(filtPixelY - filtPixelYprev);
-% % rollCmd = -Kroll*filtPixelX  + Kdr*(filtPixelX - filtPixelXprev);
-% pitchCmd = -Kpitch*filtPixelY - Kdp*diffY;
-% rollCmd = -Kroll*filtPixelX  - Kdr*diffX;
-
-% % control (PI)
-% pitchCmd = -Kpitch*filtPixelY - Kip*errorSumX;
-% rollCmd = -Kroll*filtPixelX  - Kir*errorSumY;
-
-% control (PID)
+% command
 pitchCmd = -Kpitch*filtPixelY - Kdp*diffY - Kip*errorSumX;
-rollCmd = -Kroll*filtPixelX  - Kdr*diffX - Kir*errorSumY;
 
-% correct for pitch/roll
-% pitchDesired = pitchDesired;% - pixelHeight/2*Kpitch*z*tand(theta);
-% rollDesired = rollDesired;% + pixelWidth/2*Kroll*z*tand(phi);
-
-% saturate
+% saturate, set pitch command
 ayprCmd.PitchDesiredDegrees = max(-satLimit, min(satLimit, pitchCmd));
-ayprCmd.RollDesiredDegrees = max(-satLimit, min(satLimit, rollCmd));
 
-% yaw control
-if ( hDetected )
-    desiredYawCurrent = yaw - (hAngle-90);
-    desiredYaw = (1-alpha)*desiredYaw + alpha*desiredYawCurrent;
-end
-
-if (rDetected)
-    % YAW
-    delY = yRight - yLeft;
-    desiredYaw = delY*Kyaw;
-    
-    %PITCH
-    yMid = (yRight + yLeft)/2;
-    desiredPitch = KPitchRiver*(yMid - ySetpoint);
-    
-end
-
+% set yaw comand
 ayprCmd.YawDesiredDegrees = desiredYaw;
 
 % behavior completes after time elapsed
@@ -137,32 +75,25 @@ if bhvTime >= completion.durationSec
 end
 completionFlag = 0;
 
-% for logging only:
-pitchCmdProp = -Kpitch*filtPixelY;
-rollCmdProp =  -Kroll*filtPixelX;
-
 % write log
-pFile = fopen( bhvLog  ,'a');
+pFile = fopen( riverLog  ,'a');
 fprintf(pFile,'%6.6f,',bhvTime);
-fprintf(pFile,'%6.6f,',hPixelX);
-fprintf(pFile,'%6.6f,',hPixelY);
-fprintf(pFile,'%6.6f,',filtPixelX);
+fprintf(pFile,'%6.6f,',rDetected);
+fprintf(pFile,'%6.6f,',yLeft);
+fprintf(pFile,'%6.6f,',yRight);
+fprintf(pFile,'%6.6f,',yMid);
+fprintf(pFile,'%6.6f,',yError);
 fprintf(pFile,'%6.6f,',filtPixelY);
-fprintf(pFile,'%6.6f,',hDetected);
-
-fprintf(pFile,'%6.6f,',pitchCmdProp);
+fprintf(pFile,'%6.6f,',delY);
+fprintf(pFile,'%6.6f,',rAngle);
+fprintf(pFile,'%6.6f,',desiredYaw);
+fprintf(pFile,'%6.6f,',errorSumY);
+fprintf(pFile,'%6.6f,',diffY);
 fprintf(pFile,'%6.6f,',pitchCmd);
 fprintf(pFile,'%6.6f,',ayprCmd.PitchDesiredDegrees);
+fprintf(pFile,'%6.6f,',ayprCmd.YawDesiredDegrees);
 fprintf(pFile,'%6.6f,',theta);
-
-fprintf(pFile,'%6.6f,',rollCmdProp);
-fprintf(pFile,'%6.6f,',rollCmd);
-fprintf(pFile,'%6.6f,',ayprCmd.RollDesiredDegrees);
-fprintf(pFile,'%6.6f,',phi);
-
-fprintf(pFile,'%6.6f,',hAngle);
-fprintf(pFile,'%6.6f,',yaw);
-fprintf(pFile,'%6.6f,\n',desiredYaw);
-
+fprintf(pFile,'%6.6f,\n',yaw);
 fclose(pFile);
+
 end
